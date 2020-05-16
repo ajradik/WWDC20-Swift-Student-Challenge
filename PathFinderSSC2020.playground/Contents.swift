4e80b5f8a47cd803b154c40d15d7ac7ab100d514
Artemas Radik <artemasr21@bronxvilleschool.org>
2020-05-16 15:11:43 -0400

    initial commit

---
+++ PathFinderSSC2020.playground/Contents.swift
@@ -0,0 +1,6 @@
+//Artemas J. Radik WWDC 2020 Swift Student Challenge Submission
+
+import PlaygroundSupport
+
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = ViewController()
